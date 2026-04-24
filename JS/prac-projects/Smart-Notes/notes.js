2a2b884b7bea365f629ec77c964abef2d2f3c70e
Patrick <pcano5@miners.utep.edu>
2026-04-23 18:39:47 -0600

    added more to note project

--- JS/prac-projects/Smart-Notes/notes.js
+++ JS/prac-projects/Smart-Notes/notes.js
@@ -3,16 +3,25 @@ let notesList = [{
   date: '12-22-2026'
 }];
 
-function addToArray(){
+function addNote(){
   const noteElement = document.querySelector('.input-val');
   const dateElement = document.querySelector('.date-val');
   const note = noteElement.value;
   const date = dateElement.value;
 
+  if(note === '' || date === ''){
+    return;
+  }
+
   notesList.push({
-    text: note,
+    note: note,
     date: date
   });
 
+
+
+  noteElement.value = '';
+
   console.log(notesList);
 }
+
